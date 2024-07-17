5f5e1fd86484541411b00833bb78408ace9da64c
oleggud512 <oleggud52@gmail.com>
2024-07-17 13:51:12 +0300

    init preview page

--- lib/src/features/shortest_path/presentation/preview/preview_page.dart
+++ lib/src/features/shortest_path/presentation/preview/preview_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:test_task/src/core/common/extensions/string.dart';
+import 'package:test_task/src/features/shortest_path/domain/entities/result.dart';
 
 class PreviewPage extends StatelessWidget {
-  const PreviewPage({super.key});
+  const PreviewPage({
+    super.key,
+    required this.result
+  });
+
+  final Result result;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Preview Page".hardcoded), 
+      ),
+      body: Container()
+    );
   }
 }
--- lib/src/features/shortest_path/presentation/result_list/result_list_page.dart
+++ lib/src/features/shortest_path/presentation/result_list/result_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:test_task/src/core/common/constants/sizes.dart';
 import 'package:test_task/src/core/common/extensions/string.dart';
 import 'package:test_task/src/features/shortest_path/domain/entities/result.dart';
+import 'package:test_task/src/features/shortest_path/presentation/preview/preview_page.dart';
 
 class ResultListPage extends StatelessWidget {
   const ResultListPage({
@@ -11,6 +12,12 @@ class ResultListPage extends StatelessWidget {
 
   final List<Result> results;
 
+  void onResult(BuildContext context, Result result) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return PreviewPage(result: result);
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +26,20 @@ class ResultListPage extends StatelessWidget {
       ),
       body: ListView.separated(
         itemCount: results.length,
+        padding: const EdgeInsets.all(p16),
         separatorBuilder: (context, i) => const Divider(),
         itemBuilder: (context, i) {
           final result = results[i];
-          return Padding(
-            padding: const EdgeInsets.all(p16),
-            child: Text(result.path)
+          return InkWell(
+            onTap: () => onResult(context, result),
+            child: Padding(
+              padding: const EdgeInsets.all(p16),
+              child: Text(result.path,
+                style: Theme.of(context)
+                  .textTheme.titleMedium,
+                textAlign: TextAlign.center,
+              )
+            ),
           );
         }
       )
